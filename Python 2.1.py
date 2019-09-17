bb128dd8a35e17f464b75d04d736026b26c9cbea
ZoevdHeuvel <55230364+ZoevdHeuvel@users.noreply.github.com>
2019-09-17 13:08:53 +0200

    Add files via upload

---
+++ Python 2.1.py
@@ -0,0 +1,10 @@
+naam = "Zoe"
+stad = "Oss"
+postcode = "5343VK"
+telefoonnummer = "0637561080"
+studie = "bio-informatica"
+print (naam)
+print (stad)
+print (postcode)
+print (telefoonnummer)
+print (studie)
