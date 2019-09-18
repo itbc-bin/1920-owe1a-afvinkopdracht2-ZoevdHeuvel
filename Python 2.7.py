13a357239633db30329528ebc5a9eaaed3708366
ZoevdHeuvel <55230364+ZoevdHeuvel@users.noreply.github.com>
2019-09-18 11:20:51 +0200

    Add files via upload

---
+++ Python 2.7.py
@@ -0,0 +1,13 @@
+#Zoe van den Heuvel, geschreven op 18-9-2019
+#Boek opdracht 2.7, Miles per Gallon
+#Use the formula "MPG=Miles driven/Gallons of gas used"
+#Write a program that asks the user for the number of miles driven and the gallons of gas used.
+#Then calculate the MPG and display the result
+
+Miles_driven = int(input("How many miles did you drive?"))
+Gallons_gas = int(input("How many gallons of gas did you use?"))
+
+#Calculate the MPG
+MPG = Miles_driven / Gallons_gas
+print(MPG)
+
