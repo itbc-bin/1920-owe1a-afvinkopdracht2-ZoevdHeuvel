049012021da1abde79996bf530f9621e640dfe05
ZoevdHeuvel <55230364+ZoevdHeuvel@users.noreply.github.com>
2019-09-18 11:11:34 +0200

    Add files via upload

---
+++ Python 2.5.py
@@ -0,0 +1,23 @@
+#Zoe van den Heuvel, geschreven op 18-9-2019
+#Boek opdracht 2.5, Distance traveled
+#Calculate the distance a car can travel without accidents or delays
+#Formula is Distance=SpeedxTime
+#A car is traveling 70 miles per hour
+
+#Calculate the distance the car will travel in 6 hours
+Speed = 70
+Time = 6
+Distance1 = Speed*Time
+print(Distance1)
+
+#Calculate the distance the car will travel in 10 hours
+Speed = 70
+Time = 10
+Distance2 = Speed*Time
+print(Distance2)
+
+#Calculate the distance the car will travel in 15 hours
+Speed = 70
+Time = 15
+Distance3 = Speed*Time
+print(Distance3)
