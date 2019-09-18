73533144868b5ecf4b614f88305b2a33d0a71d58
ZoevdHeuvel <55230364+ZoevdHeuvel@users.noreply.github.com>
2019-09-18 11:01:38 +0200

    Add files via upload

---
+++ Python 2.2.py
@@ -0,0 +1,10 @@
+#Zoe van den Heuvel, geschreven op 18-9-2019
+#Boek opdracht 2.2, Sales Prediction
+#Display the amount of profit with 23%
+
+#Get the total sales without profit
+sales = int(input("Wat zijn de sales?"))
+
+#Calculate the profit
+profit = sales*0.23
+print (profit)
