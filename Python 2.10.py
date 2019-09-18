4a142b17c417b551930d2a89f4709ab56349563f
ZoevdHeuvel <55230364+ZoevdHeuvel@users.noreply.github.com>
2019-09-18 11:30:21 +0200

    Add files via upload

---
+++ Python 2.10.py
@@ -0,0 +1,14 @@
+#Zoe van den Heuvel, geschreven op 18-9-2019
+#Boek opdracht 2.10, Ingredient adjuster
+#1.5 cup of sugar, 1 cup of butter and 2.75 cups of flour for 48 cookies
+#Write a program that asks how many cookies the user wants
+#And then calculates the ingredients needed
+
+aantal_cookies = int(input("Hoeveel koekjes wil je maken?")
+suiker = 1,5/48*aantal_cookies
+boter = 1/48*aantal_cookies
+bloem = 2.75/48*aantal_cookies
+print("Bloem:" , suiker)
+print("Boter:" , boter)
+print("Bloem:" , bloem)
+                     
